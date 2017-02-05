e92c2abbe88114ecf074dbaa7d9c6040af00f224
Greg Conant <greg.conant@gmail.com>
2017-02-05 14:33:32 -0500

    gc - debounce - 3 of 5 tests passing. Need to fix Context and Arguments.

--- debounce/index.js
+++ debounce/index.js
@@ -1,4 +1,16 @@
 module.exports = function(functionToRun, threshold) {
-    
-    return functionToRun;
+    var calledWithinThreshold = false;
+
+    return function() {
+        if(!calledWithinThreshold) {
+            calledWithinThreshold = true;
+            setTimeout(function() {
+                // once function is called,
+                // reset the "called" flag so it will
+                // execute the next time you call debounce
+                calledWithinThreshold = false;
+                functionToRun();
+            }, threshold);
+        }
+    }
 };
